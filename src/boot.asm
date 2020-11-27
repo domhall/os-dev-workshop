1ebe86287d8d4ed95a78282ad3e1f283afbd4e63
domhall <dhall@worth.systems>
2020-11-27 14:01:33 +0000

    Boot and hang

--- src/boot.asm
+++ src/boot.asm
@@ -1,10 +1,21 @@
 ;Constants
+FLAGS    equ  0x00000001; Ask the bootloader to page align our modules
+MAGIC    equ  0x1BADB002
+CHECKSUM equ -(MAGIC + FLAGS) 
+
 section .multiboot
+align 4
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
 
 section .bss
-
 
 section .text
 global _start:function (_start.end - _start)
 _start:
+    cli
+.hang:
+    hlt
+    jmp .hang
 .end:
